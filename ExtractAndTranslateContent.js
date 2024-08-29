7d6277929443300c77824456688ee8a4dde570bd
Thuta Min Thway <thutaminthway@onenex.co>
2024-08-29 13:40:32 +0630

    add version

--- ExtractAndTranslateContent.js
+++ ExtractAndTranslateContent.js
@@ -1,30 +1,34 @@
-const axios = require('axios');
-const cheerio = require('cheerio');
-const translate = require('@iamtraction/google-translate');
+const axios = require('axios')
+const cheerio = require('cheerio')
+const translate = require('@iamtraction/google-translate')
 const { versions } = require('./config/workpermit')
 
 class ExtractAndTranslateContent {
   constructor(url, to) {
-    this.originalInformation = {};
-    this.translatedInformation = {};
+    this.originalInformation = {}
+    this.translatedInformation = {}
     this.to = to ?? 'en'
     this.url = url
 
-    const { version, callback, fields } = this.validate(url);
+    const { version, callback, fields } = this.validate(url)
     if (!version) {
-      throw new Error('Invalid URL provided');
+      throw new Error('Invalid URL provided')
     }
 
-    this.version = version;
-    this.callback = callback;
-    this.fields = fields;
+    this.version = version
+    this.callback = callback
+    this.fields = fields
   }
 
   validate() {
     for (const [version, patternData] of Object.entries(versions)) {
-      const regex = patternData.pattern.replace(/"/g, '');
+      const regex = patternData.pattern.replace(/"/g, '')
       if (new RegExp(regex).test(this.url)) {
-        return { version, callback: patternData.callback, fields: patternData.fields };
+        return {
+          version,
+          callback: patternData.callback,
+          fields: patternData.fields
+        }
       }
     }
 
@@ -32,22 +36,22 @@ class ExtractAndTranslateContent {
   }
 
   async process() {
-    await this.extract();
-    await this.translate();
+    await this.extract()
+    await this.translate()
 
     return {
       originalInformation: this.originalInformation,
-      translatedInformation: this.translatedInformation
-    };
+      translatedInformation: this.translatedInformation,
+      version: this.version
+    }
   }
 
   async extract() {
     try {
-      const response = await axios.get(this.url);
-      const html = response.data;
-      const $ = cheerio.load(html);
+      const response = await axios.get(this.url)
+      const html = response.data
+      const $ = cheerio.load(html)
       return this.callback($)
-
     } catch (error) {
       throw new Error()
     }
@@ -59,13 +63,13 @@ class ExtractAndTranslateContent {
       to: this.to
     })
       .then((response) => {
-        this.translatedInformation = JSON.parse(response.text);
+        this.translatedInformation = JSON.parse(response.text)
       })
       .catch((error) => {
-        console.error(error);
-        process.exit(1);
-      });
+        console.error(error)
+        process.exit(1)
+      })
   }
 }
 
-module.exports = ExtractAndTranslateContent;
+module.exports = ExtractAndTranslateContent
